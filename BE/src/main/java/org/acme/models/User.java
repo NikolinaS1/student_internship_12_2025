9ee26e7feab7825d02d08efb458e08bf1077673a
Josip Novak <josipnovak7@gmail.com>
2025-12-23 11:38:15 +0100

    Changed table name in database

--- BE/src/main/java/org/acme/models/User.java
+++ BE/src/main/java/org/acme/models/User.java
@@ -2,9 +2,11 @@ package org.acme.models;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import org.acme.enums.Role;
 
 @Entity
+@Table(name="users")
 public class User extends PanacheEntity {
     private String name;
     private String password;
