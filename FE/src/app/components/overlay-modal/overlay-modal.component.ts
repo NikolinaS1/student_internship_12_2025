0e5af19b06ca5350f261aead1cf6c83a06ddf218
Domagoj Pusic <dopusic@gmail.com>
2026-01-18 12:49:37 +0100

    created case-managment

--- FE/src/app/components/overlay-modal/overlay-modal.component.ts
+++ FE/src/app/components/overlay-modal/overlay-modal.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-overlay-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './overlay-modal.component.html',
   styleUrl: './overlay-modal.component.scss'
 })
 export class OverlayModalComponent {
-
+   @Output() close = new EventEmitter<void>();
 }
